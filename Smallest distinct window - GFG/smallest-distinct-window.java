660e51c5dcf2e3195dc7bff42aa4d05ebb378d06
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-16 14:06:19 +0530

    Added solution - LeetHub

---
+++ Smallest distinct window - GFG/smallest-distinct-window.java
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            String str = br.readLine();
+            
+            Solution obj = new Solution();
+            System.out.println(obj.findSubString(str));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int findSubString( String s) {
+        // Your code goes here
+        Set<Character> count = new HashSet<>();
+        for(char c: s.toCharArray()){
+            count.add(c);
+        }
+        int min = s.length();
+        int left =0;
+        int right =0;
+       Map<Character,Integer> map = new HashMap<>();
+        for(right=0;right<s.length();right++){
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+            while(map.size()==count.size()){
+                min = Math.min(min,right-left+1);
+                map.put(s.charAt(left),map.get(s.charAt(left))-1);
+                if(map.get(s.charAt(left))==0)
+                    map.remove(s.charAt(left));
+                    left++;
+                
+            }
+        
+        }
+        return min;
+    }
+}
