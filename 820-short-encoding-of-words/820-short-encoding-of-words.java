de0da292808bbcc6efddfe112ca542fc2c4014d2
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-20 09:06:39 +0530

    Time: 1424 ms (5.52%), Space: 52.8 MB (74.85%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet(Arrays.asList(words));
+        List<String> wordlist = new ArrayList<>(set);
+        
+        Set<String> dup = new HashSet();
+        
+        for(int i=0;i<wordlist.size();i++){
+            for(int j=0;j<wordlist.size();j++){
+                if(i!=j){
+                    if(wordlist.get(i).endsWith(wordlist.get(j))){
+                        dup.add(wordlist.get(j));
+                    }
+                }
+            }
+        }
+       
+        int count =0;
+        for(String word:wordlist){
+            if(!dup.contains(word)){
+                count += word.length()+1;
+                 System.out.println(count);
+            }
+        }
+        return count;
+    }
+}
