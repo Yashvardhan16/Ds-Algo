928ade727ac66fc558d704fb6542f1d8436d9641
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-23 09:51:00 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (21.86%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<List<Integer>> generate(int n) {
         List<List<Integer>> list = new ArrayList<>();
-        if(n==0) return list;
-        List<Integer> first = new ArrayList();
-        first.add(1);
-        list.add(first);
+                if(n==0) return list;
+        List<Integer> curr = new ArrayList<>();
+        curr.add(1);
+        list.add(curr);
         
         for(int i=1;i<n;i++){
             List<Integer> prev = list.get(i-1);
-            List<Integer> row = new ArrayList();
+            List<Integer> row = new ArrayList<>();
             
             row.add(1);
-            
             for(int j=1;j<i;j++){
                 row.add(prev.get(j-1)+prev.get(j));
             }
@@ -19,6 +18,5 @@ class Solution {
             list.add(row);
         }
         return list;
-        
     }
 }
