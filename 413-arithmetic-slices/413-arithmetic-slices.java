9ea3af2f04b8448528122c24f728a7101b1c285b
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-25 14:47:21 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (5.81%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -3,7 +3,7 @@ class Solution {
         int count =0;
         int n = nums.length;
         if(n<3) return 0;
-        int[] dp = new int [n+1];
+        int[] dp = new int [n];
         for(int i=2;i<n;i++){
             if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
                 dp[i] = dp[i-1] +1;
