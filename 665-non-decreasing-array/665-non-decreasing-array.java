0ecd121d4912699b8793b7affc919563a909a485
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-25 09:26:45 +0530

    Time: 1 ms (97.53%), Space: 54.4 MB (21.44%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -3,9 +3,7 @@ class Solution {
 int count =0;
         for(int i=1,prev=nums[0];i<nums.length;i++){
             if(prev>nums[i]){
-                if(++count>1) return false;
-            
-            
+                if(count++>0) return false;
             if(i-2>=0 && nums[i-2]>nums[i]) continue;
             }
             prev = nums[i];
