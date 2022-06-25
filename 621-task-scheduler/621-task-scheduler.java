8b4269caa7e1149c4a96a2f5ca0015e18eac604a
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-25 11:05:18 +0530

    Time: 2 ms (98.80%), Space: 43.5 MB (87.97%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,18 +1,17 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-         int[] count = new int[26];
-        int max = 0;
-
-        for (char i : tasks) {
-            count[i-'A']++;
-            max = Math.max(max, count[i-'A']);
+        int[] count = new int[26];
+        int max =0;
+        for(char c:tasks){
+            count[c-'A']++;
+            max =Math.max(max,count[c-'A']);
         }
-        int maxcount = 0;
-        for (int i : count) {
-            if (i == max)
+        int maxcount =0;
+        for(int i:count){
+            if(i==max){
                 maxcount++;
+            }
         }
-
-        return Math.max(tasks.length,(max-1)*(n+1)+maxcount);
+        return Math.max(tasks.length,(n+1)*(max-1)+maxcount);
     }
 }
