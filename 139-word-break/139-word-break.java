d7b1e36a496833088f54e67e0286d202d849b463
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-27 14:30:45 +0530

    Time: 22 ms (9.93%), Space: 47.5 MB (23.31%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -4,11 +4,10 @@ class Solution {
        if(wordDict.contains(s)) return true;
         
         if(map.containsKey(s)) return map.get(s);
-           Set<String> set = new HashSet(wordDict);
-
+        
         for(int i=0;i<s.length();i++){
             String left = s.substring(0,i);
-            if(set.contains(left) && wordBreak(s.substring(i),wordDict)){
+            if(wordDict.contains(left) && wordBreak(s.substring(i),wordDict)){
                 map.put(left,true);
                 return true;
             }
