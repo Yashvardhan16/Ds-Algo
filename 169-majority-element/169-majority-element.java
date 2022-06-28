32b91855fecb781c626f11ef0504ec2f1436447d
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-28 10:20:23 +0530

    Time: 3 ms (62.63%), Space: 56.1 MB (46.79%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,12 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int max =0;
         int count =0;
+        int max =0;
         for(int i:nums){
-            if(count==0){
-                max =i;
-            }
-             if(i==max){
+            if(count==0)
+            max =i;
+            
+            if(i==max){
                 count++;
             }else{
                 count--;
