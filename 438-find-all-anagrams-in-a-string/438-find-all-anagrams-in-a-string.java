035a8d9de5afc8838c35e3c3a58a5d9b460d2ea6
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-29 13:17:14 +0530

    Time: 14 ms (65.17%), Space: 47.7 MB (64.23%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,28 +1,22 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int n = s.length();
-        int m  = p.length();
         List<Integer> list = new ArrayList();
+        int n = s.length();
+        int m = p.length();
         if(m>n) return list;
-        
-        int[] freq1 = new int[26];
-         int[] freq2 = new int[26];
-        
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
         for(int i=0;i<m;i++){
-            freq1[p.charAt(i)-'a']++;
-            freq2[s.charAt(i)-'a']++;
+            count1[s.charAt(i)-'a']++;
+            count2[p.charAt(i)-'a']++;
         }
-      
-        if(Arrays.equals(freq1,freq2)){
-            list.add(0);
-        }
+        
+        if(Arrays.equals(count1,count2)) list.add(0);
         
         for(int i=m;i<n;i++){
-             freq2[s.charAt(i)-'a']++;
-             freq2[s.charAt(i-m)-'a']--;
-            if(Arrays.equals(freq1,freq2)){
-                list.add(i-m+1);
-            }
+        count1[s.charAt(i) -'a']++;
+        count1[s.charAt(i-m) -'a']--;
+        if(Arrays.equals(count1,count2)) { list.add(i-m+1);}
         }
         return list;
     }
