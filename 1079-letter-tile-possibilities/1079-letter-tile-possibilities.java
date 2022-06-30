000886bf77613f2b2753c0921b4b7cf86096b34a
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-30 13:52:49 +0530

    Time: 88 ms (13.04%), Space: 56 MB (8.58%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTilePossibilities(String s) {
+     Set<String> set = new HashSet();
+        boolean[] vis = new boolean[s.length()];
+        helper(s,"",set,vis);
+        return set.size()-1;
+    }
+    void helper(String s, String curr,Set<String> set, boolean[] vis){
+        set.add(curr);
+        for(int i=0;i<s.length();i++){
+            if(!vis[i]){
+                vis[i] = true;
+                helper(s,curr+s.charAt(i),set,vis);
+                vis[i] = false;
+            }
+        }
+    }
+}    
+    
