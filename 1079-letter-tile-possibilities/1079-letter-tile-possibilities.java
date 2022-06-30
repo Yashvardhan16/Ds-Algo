1949ef28fe66698bd6c2aefa324b7d801be1e339
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-06-30 13:55:03 +0530

    Time: 48 ms (33.75%), Space: 54.3 MB (30.09%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -8,6 +8,8 @@ class Solution {
     void helper(String s, String curr,Set<String> set, boolean[] vis){
         set.add(curr);
         for(int i=0;i<s.length();i++){
+            if (vis[i] || i > 0 && s.charAt(i) == s.charAt(i-1) && !vis[i - 1])
+                continue;
             if(!vis[i]){
                 vis[i] = true;
                 helper(s,curr+s.charAt(i),set,vis);
