551813d525af41d43f673164be28d7aa1a97c86f
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-02 11:44:59 +0530

    Time: 22 ms (5.26%), Space: 42.4 MB (43.39%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String reorganizeString(String s) {
+        Map<Character,Integer> map =new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        System.out.println(map);
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> map.get(b)-map.get(a));
+        pq.addAll(map.keySet());
+           System.out.println(pq);
+        while(pq.size()>1){
+            char first = pq.poll();
+                char second = pq.poll();
+            sb.append(first);
+            sb.append(second);
+            map.put(first,map.get(first)-1);
+             map.put(second,map.get(second)-1);
+            if(map.get(first)>0){
+                pq.add(first);
+            }
+            
+            if(map.get(second)>0){
+                pq.add(second);
+            }
+            }
+            if(!pq.isEmpty()){
+                char last = pq.poll();
+                if(map.get(last)>1){
+                    return "";
+                }
+                sb.append(last);
+            }
+        
+        return sb.toString();
+    }
+}
