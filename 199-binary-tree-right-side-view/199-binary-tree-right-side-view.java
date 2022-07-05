dc50df6f90388e60fffc19f7f21ecb5cb37264cd
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-05 13:34:08 +0530

    Time: 5 ms (11.04%), Space: 43.4 MB (6.03%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -16,17 +16,16 @@
 class Solution {
     List<Integer> list = new ArrayList();
     public List<Integer> rightSideView(TreeNode root) {
-        if(root==null) return Collections.emptyList();
+        if(root==null) return new ArrayList<>();
         helper(root,0);
         return list;
     }
     void helper(TreeNode root,int depth){
-        if(root==null) return;
+        if(root!=null){
+        if(depth==list.size()) list.add(root.val);
         
-        if(depth==list.size()){
-        list.add(root.val);
-        }
         helper(root.right,depth+1);
         helper(root.left,depth+1);
+        }
     }
 }
