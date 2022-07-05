22d428f8a1a1a28b220b526ae5583aa5229c808d
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-05 18:28:22 +0530

    Time: 52 ms (21.81%), Space: 76.7 MB (10.02%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.java
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minMovesToMakePalindrome(String s) {
-
-int res = 0;
-    while (s.length() > 0) {
-        int i = s.indexOf(s.charAt(s.length() - 1));
-        if (i == s.length() - 1) res += i / 2;
-        else {
-            res += i;
-            s = s.substring(0, i) + s.substring(i + 1);
+        int ans =0;
+        while(s.length()>0){
+           int i = s.indexOf(s.charAt(s.length() - 1));
+        if (i == s.length() - 1) ans += i / 2;
+            else{
+                ans+=i;
+               s= s.substring(0,i)+s.substring(i+1);
+            }
+          s=  s.substring(0,s.length()-1);
         }
-        s = s.substring(0, s.length() - 1);
+        return ans;
     }
-    return res;
-}
 }
