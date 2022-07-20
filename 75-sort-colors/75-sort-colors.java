aac5b031293f58e1ae6925a9c19e62ee51f65ba3
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-20 19:17:16 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (62.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -2,19 +2,20 @@ class Solution {
     public void sortColors(int[] nums) {
         int lo=0;
         int hi = nums.length-1;
-        int i=0;
-        while(i<=hi){
-            if(nums[i]==0){
-                nums[i] = nums[lo];
+        int index=0;
+        while(index<=hi){
+            if(nums[index]==0){
+                nums[index]=nums[lo];
                 nums[lo]=0;
-                lo++;
-            }else if(nums[i]==2){
-                nums[i] = nums[hi];
-                nums[hi]= 2;
+              lo++;
+            }else if(nums[index]==2){
+                nums[index] =nums[hi];
+                nums[hi] = 2;
                 hi--;
-                i--;
+                index--;
             }
-            i++;
+            index++;
         }
+       
     }
 }
