2982438b2192294ebf362ef493021699e6a0a0ef
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-24 12:53:24 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (65.73%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo =0;
+        int hi = nums.length-1;
+        if(target>nums[hi]) return nums.length;
+        
+        while(lo<hi){
+            int mid = (lo+hi)/2;
+            
+            if(nums[mid]==target) 
+                return mid;
+            
+            else if(nums[mid]>target){
+                hi = mid;
+            }else {
+                lo=mid+1;
+            }
+        }
+        return lo;
+    }
+}
