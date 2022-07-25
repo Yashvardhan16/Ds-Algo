85445e0d1719b6292de8333120cdcaf20412737c
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-25 10:04:13 +0530

    Time: 152 ms (64.86%), Space: 41.8 MB (78.48%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,28 +1,28 @@
 class Solution {
-    public boolean exist(char[][] a, String word) {
-        int n = a.length;
-        int m = a[0].length;
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(a[i][j]==word.charAt(0) && helper(a,word,i,j,0)){
+                if(board[i][j]==word.charAt(0) && helper(board,word,i,j,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    boolean helper(char[][] a,String word,int i,int j,int start){
-        if(start==word.length()) return true;
- if(i<0||j<0||i>a.length-1||j>a[0].length-1||a[i][j]!=word.charAt(start)){
+    boolean helper(char[][] board,String word,int i,int j,int start){
+                if(start==word.length()) return true;
+
+        if(i<0||j<0||i>board.length-1||j>board[0].length-1||board[i][j]!=word.charAt(start)){
             return false;
         }
         
-        char temp = a[i][j];
-        a[i][j] = '*';
-       boolean dup= helper(a,word,i+1,j,start+1) || helper(a,word,i-1,j,start+1)||
-         helper(a,word,i,j+1,start+1)||
-         helper(a,word,i,j-1,start+1);
-      a[i][j]=  temp ;
+        char temp = board[i][j];
+        board[i][j] = '*';
+        boolean dup = helper(board,word,i-1,j,start+1)||helper(board,word,i+1,j,start+1)||helper(board,word,i,j-1,start+1)||helper(board,word,i,j+1,start+1);
+        board[i][j] = temp;
         return dup;
     }
 }
