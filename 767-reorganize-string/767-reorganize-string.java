3185da06a9b31a1a3144d3636a76caf473e99730
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-07-28 11:11:55 +0530

    Time: 11 ms (17.31%), Space: 42.7 MB (22.80%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character,Integer> map =new HashMap<>();
+      Map<Character,Integer> map =new HashMap<>();
         for(char c:s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
@@ -9,27 +9,26 @@ class Solution {
         pq.addAll(map.keySet());
         while(pq.size()>1){
             char first = pq.poll();
-                char second = pq.poll();
+            char second = pq.poll();
             sb.append(first);
             sb.append(second);
             map.put(first,map.get(first)-1);
-             map.put(second,map.get(second)-1);
+            map.put(second,map.get(second)-1);
+            
             if(map.get(first)>0){
                 pq.add(first);
             }
-            
-            if(map.get(second)>0){
+             if(map.get(second)>0){
                 pq.add(second);
             }
-            }
-            if(!pq.isEmpty()){
+        }
+         if(!pq.isEmpty()){
                 char last = pq.poll();
                 if(map.get(last)>1){
                     return "";
                 }
                 sb.append(last);
             }
-        
         return sb.toString();
     }
 }
