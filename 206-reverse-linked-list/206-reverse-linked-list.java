4d40ef84862838bb794dccdad7bd325d7b137052
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-02 19:24:44 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (77.04%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,14 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-    if(head==null) return null;
-        ListNode prev =null;
-        while(head!=null){
+        if(head==null) return head;
+        ListNode prev = null;
+        while(head.next!=null){
             ListNode temp = head.next;
             head.next = prev;
             prev = head;
-           head= temp;
+            head = temp;
         }
-        return prev;
+        head.next = prev;
+        return head;
     }
 }
