31cd2294f8356477357a1e8746e37831e33d2664
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-03 12:06:39 +0530

    Time: 4 ms (79.51%), Space: 42 MB (65.22%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -5,19 +5,18 @@ class Solution {
             char c = s.charAt(0);
             int count =1;
             StringBuilder sb = new StringBuilder();
-            
             for(int j=1;j<s.length();j++){
                 if(c!=s.charAt(j)){
                     sb.append(count);
                     sb.append(c);
                     count =0;
-                    c = s.charAt(j);
+                    c=s.charAt(j);
                 }
                 count++;
             }
             sb.append(count);
             sb.append(c);
-            s = sb.toString();
+            s= sb.toString();
         }
         return s;
     }
