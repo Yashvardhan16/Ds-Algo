686fa859ce1dbbcbc3ea0e045ca1d77a5d5fbc96
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-10 17:50:58 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (85.84%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,15 @@
+class Solution {
+    Map<Integer,Integer> map = new HashMap();
+    public int numTrees(int n) {
+        if(n==0||n==1) return 1;
+        if(map.containsKey(n))
+            return map.get(n);
+        map.put(1,1);
+        int c=0;
+        for(int i=1;i<=n;i++){
+            c+=numTrees(i-1)*numTrees(n-i);
+        }
+        map.put(n,c);
+        return map.get(n);
+    }
+}
