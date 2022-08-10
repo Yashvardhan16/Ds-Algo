58d5ad9af703ad0011e40d2d9658fd6b321369ee
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-10 17:19:40 +0530

    Time: 15 ms (78.71%), Space: 78.8 MB (37.77%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -11,46 +11,42 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if(head==null||head.next==null) return head;
-        ListNode temp =head;
+        ListNode temp = head;
         ListNode slow = head;
-        ListNode fast =head;
-        
+        ListNode fast = head;
         while(fast!=null && fast.next!=null){
-            temp = slow;
-            slow = slow.next;
-            fast = fast.next.next;
+            temp = slow ;
+            slow = slow.next ;
+             fast = fast.next.next;
         }
-        
         temp.next = null;
-        ListNode left =  sortList(head);
-        ListNode right = sortList(slow);
+        ListNode left = sortList(head);
+        ListNode right=  sortList(slow);
         return merge(left,right);
     }
-    
-    ListNode merge(ListNode l1, ListNode l2){
-        
+    ListNode merge(ListNode l1,ListNode l2){
         ListNode sorted = new ListNode(-1);
         ListNode curr = sorted;
-        
         while(l1!=null && l2!=null){
             if(l1.val<l2.val){
                 curr.next = l1;
                 l1 = l1.next;
-            }
-            else{
+            }else{
                 curr.next = l2;
-                l2=l2.next;
+                l2 = l2.next;
             }
             curr = curr.next;
         }
         if(l1!=null){
-                curr.next = l1;
+            curr.next = l1;
                 l1 = l1.next;
-            }
+            curr = curr.next;
+        }
         if(l2!=null){
-                curr.next = l2;
+             curr.next = l2;
                 l2 = l2.next;
-            }
+            curr = curr.next;
+        }
         return sorted.next;
     }
 }
