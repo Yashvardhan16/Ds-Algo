5b9d335cc4ffb0e817c300a1224814973558a3c3
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-14 17:41:44 +0530

    Time: 7 ms (70.78%), Space: 42.9 MB (66.71%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,22 +1,20 @@
 class Solution {
-    public String multiply(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        if(m==0||n==0||s.equals("0")||t.equals("0")) return "0";
-        
-        if(s.equals("1")) return t;
-        if(t.equals("1")) return s;
+    public String multiply(String num1, String num2) {
+        int n = num1.length();
+        int m = num2.length();
+        if(n==0||m==0||num1.equals("0")||num2.equals("0")) return "0";
         int[] ans = new int[n+m];
+        
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-                int mul = (s.charAt(i)-'0') * (t.charAt(j)-'0');
+                int mul = (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
                 int sum = mul + ans[i+j+1];
                 ans[i+j+1] = sum%10;
                 ans[i+j] += sum/10;
             }
         }
         StringBuilder sb = new StringBuilder();
-        for(int i: ans){
+        for(int i:ans){
             if(i==0 && sb.length()==0) continue;
             sb.append(i);
         }
