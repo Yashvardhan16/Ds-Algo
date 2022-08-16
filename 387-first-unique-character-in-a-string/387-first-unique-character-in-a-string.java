5ee0524ccf377102e49081e94906341d8858046a
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-16 10:52:09 +0530

    Time: 14 ms (71.64%), Space: 48.8 MB (61.88%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,13 +1,13 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character,Integer> map = new HashMap();
+        int[] count = new int[26];
         
         for(int i=0;i<s.length();i++){
-          map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);  
+            count[s.charAt(i)-'a']++;
         }
-        
+        System.out.println(count[0]);
         for(int i=0;i<s.length();i++){
-            if(map.get(s.charAt(i))==1) return i;
+            if(count[s.charAt(i)-'a']==1) return i;
         }
         return -1;
     }
