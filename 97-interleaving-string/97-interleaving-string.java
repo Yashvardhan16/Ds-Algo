a3360d229cdeb99641122056ebe9918fe73f9545
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-16 11:06:29 +0530

    Time: 5 ms (55.56%), Space: 42.4 MB (43.62%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -6,15 +6,15 @@ class Solution {
         
         for(int i=0;i<=s1.length();i++){
             for(int j=0;j<=s2.length();j++){
-                if(i==0&&j==0) dp[i][j] =true;
-            else if(i==0){
-     dp[i][j] = dp[i][j-1] && s2.charAt(j-1)==s3.charAt(i+j-1);
-            }
-            else if(j==0){
-  dp[i][j] = dp[i-1][j] && s1.charAt(i-1)==s3.charAt(i+j-1);
-            }else{
-                dp[i][j] = dp[i][j-1] && s2.charAt(j-1)==s3.charAt(i+j-1) ||dp[i-1][j] && s1.charAt(i-1)==s3.charAt(i+j-1);
-            }
+                if(i==0 && j==0){
+                    dp[i][j] = true;
+                }else if(i==0){
+                    dp[i][j] = dp[i][j-1] && s2.charAt(j-1)==s3.charAt(i+j-1);
+                }else if(j==0){
+                    dp[i][j] = dp[i-1][j] && s1.charAt(i-1)==s3.charAt(i+j-1);
+                }else{
+                    dp[i][j] = dp[i][j-1] && s2.charAt(j-1)==s3.charAt(i+j-1) || dp[i-1][j] && s1.charAt(i-1)==s3.charAt(i+j-1);
+                }
             }
         }
         return dp[s1.length()][s2.length()];
