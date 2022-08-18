088a7014630b7fe7a696a9834ba9ef69e960830b
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-08-18 19:08:37 +0530

    Time: 17 ms (68.15%), Space: 135.3 MB (83.10%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> list = new ArrayList();
-        helper(list,new ArrayList<>(),s);
+        helper(list,s,new ArrayList());
         return list;
     }
-    void helper(List<List<String>> list,List<String> res,String s){
-        if(s.length()==0){
+    void helper(List<List<String>> list ,String s, List<String> res){
+         if(s.length()==0){
             list.add(new ArrayList<>(res));
         }
-        
         for(int i=0;i<s.length();i++){
-            if(isvalid(s.substring(0,i+1))){
+            if(isValid(s.substring(0,i+1))){
                 res.add(s.substring(0,i+1));
-                helper(list,res,s.substring(i+1));
+                helper(list,s.substring(i+1),res);
                 res.remove(res.size()-1);
             }
         }
     }
-    boolean isvalid(String s){
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
-                return false;
+        
+        boolean isValid(String s){
+            for(int i=0;i<s.length()/2;i++){
+                if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
-    }
+    
 }
