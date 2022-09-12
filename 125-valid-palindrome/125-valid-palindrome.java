49b35fd7aaabca1fdb97ee39f91492a47c80d4a6
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-12 16:20:20 +0530

    Time: 22 ms (43.07%), Space: 45.2 MB (39.63%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String temp) {
+        if(temp==""||temp==" ") return true;
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack();
+        String s= temp.toLowerCase();
+        for(int i=0;i<s.length();i++){
+       char ch = (char)s.charAt(i);
+     if(ch>='0' && ch<='9'||ch>='a' && ch<='z'){
+                sb.append(s.charAt(i));
+                st.push(s.charAt(i));
+     }
+        }
+         System.out.println(sb);
+        for(int i=0;i<sb.length();i++){
+            if(st.pop()!=sb.charAt(i)){
+                return false;
+            }
+        }
+       
+        return true;
+    }
+}
