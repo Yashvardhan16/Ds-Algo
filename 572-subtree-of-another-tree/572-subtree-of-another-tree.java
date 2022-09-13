478628eac7087fde3132d21e1be1035dbd758898
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-13 18:13:46 +0530

    Time: 5 ms (39.66%), Space: 46.8 MB (32.31%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -16,18 +16,18 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode sub) {
         if(root==null||sub==null) return false;
-        if(root==sub) return true;
-        if(dfs(root,sub)) return true;
+         if(root==sub) return true;
+        if(helper(root,sub)) return true;
         return isSubtree(root.left,sub)||isSubtree(root.right,sub);
-        
     }
-    boolean dfs(TreeNode root, TreeNode sub){
-        if(root==sub) return true;
-         if(root==null||sub==null) return false;
-
-        if(sub.val!=root.val) return false;
+    boolean helper(TreeNode root,TreeNode sub){
+          if(root==sub) return true;
+       if(root==null || sub==null) return false;
+       
+       if(sub.val!=root.val) return false;
         
-        return dfs(root.left,sub.left) && dfs(root.right,sub.right);
-        
+       return  helper(root.left,sub.left) &&
+        helper(root.right,sub.right);
+      
     }
 }
