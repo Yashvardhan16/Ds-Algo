84e183eb6b3c814919c0444ba95b7157573b76c5
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-14 13:59:10 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (92.08%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        helper(ans,new StringBuilder(),0,0,n);
+        return ans;
+    }
+    void helper(List<String> ans,StringBuilder sb,int open,int close,int n){
+        if(sb.length()==2*n){
+            ans.add(sb.toString());
+            return;
+        }
+        
+        if(open<n){
+            sb.append('(');
+            helper(ans,sb,open+1,close,n);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close<open){
+            sb.append(')');
+            helper(ans,sb,open,close+1,n);
+              sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
