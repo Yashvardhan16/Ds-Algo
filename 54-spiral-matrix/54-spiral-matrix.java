3ee350cf90d521bf2b6a2d66312927a6bb014d98
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-15 13:30:50 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (46.74%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,35 +1,36 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-         List<Integer> list = new ArrayList<>();
-        if(matrix.length==0) return list;
+        int n = matrix.length,m=matrix[0].length;
         
-        int rstart=0;
-        int rend = matrix.length-1;
-        int cstart=0;
-        int cend = matrix[0].length-1;
-        while(rstart<=rend && cstart<=cend){
-        for(int i=cstart;i<=cend;i++){
-            list.add(matrix[rstart][i]);
-        }
-            rstart++;
-            for(int i=rstart;i<=rend;i++){
-            list.add(matrix[i][cend]);
-        }
-            cend--;
-            
-            if(rstart<=rend){
-                for(int i = cend;i>=cstart;i--){
-                    list.add(matrix[rend][i]);
-                }
+        int rowb=0;
+        int rowe=n-1;
+        int colb=0;
+        int cole=m-1;
+        List<Integer> list = new ArrayList();
+        while(rowb<=rowe && colb<=cole){
+            for(int i=colb;i<=cole;i++){
+                list.add(matrix[rowb][i]);
             }
-            rend--;
-            if(cstart<=cend){
-                for(int i = rend;i>=rstart;i--){
-                    list.add(matrix[i][cstart]);
-                }
+            rowb++;
+            for(int i=rowb;i<=rowe;i++){
+                list.add(matrix[i][cole]);
             }
-            cstart++;
-            
+            cole--;
+        
+        if(rowb<=rowe){
+            for(int i=cole;i>=colb;i--){
+                list.add(matrix[rowe][i]);
+            }
+           
+        }
+             rowe--;
+        if(colb<=cole){
+            for(int i=rowe;i>=rowb;i--){
+                list.add(matrix[i][colb]);
+            }
+          
+        }
+              colb++;
         }
         return list;
     }
