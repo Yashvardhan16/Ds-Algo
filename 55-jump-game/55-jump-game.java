6e64bd96b0b4f6aef32c764a28a95121d2e56366
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-15 12:53:51 +0530

    Time: 9 ms (31.76%), Space: 68.2 MB (40.39%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-         int steps =0;
+        if(nums.length<=1) return true;
+        int n = nums.length;
+        int steps =0;
         for(int i=0;i<=steps;i++){
-            steps = Math.max(steps,nums[i]+i);
+           steps = Math.max(steps,nums[i]+i);
             
-            if(steps>=nums.length-1){
-                return true;
-            }
+            if(steps>nums.length-2) return true;
         }
         return false;
     }
