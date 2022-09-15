3694e5c086001b4d2f62d667a4dfd7a1743e9548
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-15 13:21:52 +0530

    Time: 1 ms (66.44%), Space: 42.7 MB (21.82%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,34 +1,33 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] a) {
-        int n=a.length;
-        int m = a[0].length;
-        int[][] dp = new int[n][m];
-        for(int i=0;i<n;i++){
-            if(a[i][0]==1){
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+       int[][] dp = new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
+        
+        for(int i=0;i<obstacleGrid.length;i++){
+            if(obstacleGrid[i][0]==1){
                 dp[i][0] =0;
                 break;
             }else{
-                dp[i][0] = 1;
+                dp[i][0] =1;
             }
         }
-        for(int i=0;i<m;i++){
-            if(a[0][i]==1){
+         for(int i=0;i<obstacleGrid[0].length;i++){
+            if(obstacleGrid[0][i]==1){
                 dp[0][i] =0;
                 break;
             }else{
-                dp[0][i] = 1;
+                dp[0][i] =1;
             }
         }
         
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(a[i][j]==1)dp[i][j] = 0;
-                
-                else
-                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
-            
+        for(int i=1;i<obstacleGrid.length;i++){
+            for(int j=1;j<obstacleGrid[0].length;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }else{
+                   dp[i][j] = dp[i-1][j] + dp[i][j-1]; 
+                }
             }
         }
-        return dp[n-1][m-1];
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1];
     }
 }
