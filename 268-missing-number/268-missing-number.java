49853dd7bb42020674f34f4a99ba6981b0990c0e
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-16 13:32:46 +0530

    Time: 17 ms (5.20%), Space: 54.2 MB (5.79%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        
+        for(int i=0;i<=nums.length;i++){
+            if(!map.containsKey(i)) return i;
+        }
+        return 0;
+    }
+}
