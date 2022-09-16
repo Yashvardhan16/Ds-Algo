8d65e836cf7c8093449eb38e4d5df69490692af9
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-16 13:25:53 +0530

    Time: 12 ms (10.00%), Space: 54.9 MB (11.61%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -10,24 +10,47 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode[] left = new ListNode[1];
-        left[0] = head;
-        reorder(left,head);
-    }
-    void reorder(ListNode[] left, ListNode right){
-        if(right==null) return;
-        
-        reorder(left,right.next);
-        if(left[0].next!=null){
-            ListNode leftnext = left[0].next ;
-            left[0].next = right;
-            right.next = leftnext;
-            left[0] = leftnext;
-            
+        if(head==null||head.next==null) return ;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        prev.next = null;
         
-        if(left[0].next==right){
-            left[0].next=null;
+        ListNode second = reverse(slow);
+        merge(curr,second);
+    }
+    
+    ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+        
+    }
+    void merge(ListNode l1,ListNode l2){
+        while(l1!=null){
+            ListNode l1_next=l1.next;
+            ListNode l2_next=l2.next;
+            l1.next=l2;
+            
+            if(l1_next==null){ break;}
+            
+            l2.next = l1_next;
+            
+            l1=l1_next;
+            l2=l2_next;
         }
     }
 }
