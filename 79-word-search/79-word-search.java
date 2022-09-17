3ebfe851118e4fa4df068a1bcf15ca2375f22ba6
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-17 19:17:10 +0530

    Time: 192 ms (45.52%), Space: 42 MB (64.15%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,8 +1,9 @@
 class Solution {
+    boolean[][] visited;
     public boolean exist(char[][] board, String word) {
         int n = board.length;
             int m= board[0].length;
-        
+        visited = new boolean[n][m];
         int count =0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -14,13 +15,12 @@ class Solution {
     
     boolean function(char[][] board,int i,int j,int start,String word){
         if(start==word.length()) return true;
-        if(i<0||j<0||i>board.length-1||j>board[0].length-1||board[i][j]!=word.charAt(start)) {return false;}
-        
-        char temp = board[i][j];
-        board[i][j] = '*';
-        boolean dup = function(board,i+1,j,start+1,word)||function(board,i-1,j,start+1,word)||function(board,i,j+1,start+1,word)||function(board,i,j-1,start+1,word);
-        board[i][j] = temp;
-        return dup;
+        if(i<0||j<0||i>board.length-1||j>board[0].length-1||board[i][j]!=word.charAt(start)||visited[i][j]) {return false;}
+    
+        visited[i][j] = true;
+       if(function(board,i+1,j,start+1,word)||function(board,i-1,j,start+1,word)||function(board,i,j+1,start+1,word)||function(board,i,j-1,start+1,word)) return true;
+      visited[i][j] = false;
+        return false;
         
     }
 }
