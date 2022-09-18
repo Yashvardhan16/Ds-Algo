bc9b0a9a22fed9a96cb97448d3f2b2669262524f
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-18 14:31:43 +0530

    Time: 13 ms (34.03%), Space: 43.8 MB (58.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        int[] count = new int[26];
+       if(s.equals(t)) return true;
         
+        int[] c1= new int[26];
         for(char c:s.toCharArray()){
-            count[c-'a']++;
+            c1[c-'a']++;
         }
-         for(char c:t.toCharArray()){
-            count[c-'a']--;
+        for(char c:t.toCharArray()){
+            c1[c-'a']--;
         }
-        
-        for(int i=0;i<26;i++){
-            if(count[i]>0) return false;
+        for(int i=0;i<s.length();i++){
+            if(c1[s.charAt(i)-'a']>0) return false;
         }
         return true;
     }
