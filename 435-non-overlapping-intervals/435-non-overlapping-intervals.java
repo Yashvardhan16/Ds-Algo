29a7499f46f41f286f0db69a61dfbdcc59cd243f
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-21 17:38:13 +0530

    Time: 119 ms (28.33%), Space: 105.4 MB (15.88%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,15 +1,14 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if(intervals.length==0) return 0;
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
-        int prevend = intervals[0][1];
         int count =0;
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        int prev = intervals[0][1];
         for(int i=1;i<intervals.length;i++){
-           if(intervals[i][0]<prevend){
-               count++;
-           }
-            else{
-               prevend = intervals[i][1];
+            if(intervals[i][0]<prev){
+                count++;
+                
+            }else{
+                prev=intervals[i][1];
             }
         }
         return count;
