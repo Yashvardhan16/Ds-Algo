8e6ca66efa9ccdc29a9010b964e001ebc5e7b8ff
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-09-22 14:07:33 +0530

    Time: 12 ms (6.52%), Space: 57.9 MB (38.10%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -5,11 +5,10 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             ans[i] = ans[i-1]*nums[i-1];
         }
-        int right_prod = 1;
-        
-        for(int i=ans.length-1;i>=0;i--){
+        int right_prod =1;
+        for(int i=nums.length-1;i>=0;i--){
             ans[i] = ans[i]*right_prod;
-            right_prod = right_prod*nums[i];
+            right_prod*=nums[i];
         }
         return ans;
     }
