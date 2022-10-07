51b579920031f5b37ac2baf1a966e7aa3b2a3da7
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-10-07 10:50:53 +0530

    Time: 1 ms (82.82%), Space: 42.7 MB (50.02%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,17 +14,15 @@
  * }
  */
 class Solution {
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
         if(root==null) return;
-        flatten(root.left);
-        flatten(root.right);
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        root.left = null;
-        root.right = left;
-        while(root.right!=null)
-            root = root.right;
         
-        root.right = right;
+        flatten(root.right);
+        flatten(root.left);
+        
+        root.left = null;
+        root.right = prev;
+        prev = root;
     }
 }
