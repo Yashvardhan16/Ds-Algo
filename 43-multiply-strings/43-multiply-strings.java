a6bfeec91242a3fcd80234dec4db1999ae24bce6
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-10-08 13:58:08 +0530

    Time: 22 ms (28.67%), Space: 44.1 MB (31.45%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,23 +1,24 @@
 class Solution {
-    public String multiply(String num1, String num2) {
-        int n = num1.length();
-        int m = num2.length();
-        if(n==0||m==0||num1.equals("0")||num2.equals("0")) return "0";
-        int[] ans = new int[n+m];
+    public String multiply(String a, String b) {
+        int n = a.length();
+        int m = b.length();
+        if(n==0||m==0||a.equals("0")||b.equals("0")) return "0";
         
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                int mul = (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
-                int sum = mul + ans[i+j+1];
-                ans[i+j+1] = sum%10;
-                ans[i+j] += sum/10;
-            }
-        }
-        StringBuilder sb = new StringBuilder();
+        int[] ans = new int[n+m];
+       for(int i=n-1;i>=0;i--){
+           for(int j=m-1;j>=0;j--){
+               int mul = (a.charAt(i)-'0')*(b.charAt(j)-'0');
+               int sum = mul+ ans[i+j+1];
+               ans[i+j+1] = sum%10;
+               ans[i+j]+=sum/10;
+           }
+       }
+
+        String res="";
         for(int i:ans){
-            if(i==0 && sb.length()==0) continue;
-            sb.append(i);
+            if(i==0&&res.length()==0) continue;
+            res+=i;
         }
-        return sb.toString().trim();
+        return res;
     }
 }
