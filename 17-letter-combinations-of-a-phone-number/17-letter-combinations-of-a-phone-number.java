c2e20e01dd057651d4837159bbe06a6961c20d63
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-10-08 19:49:43 +0530

    Time: 1 ms (96.58%), Space: 42.8 MB (51.46%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,22 +1,23 @@
 class Solution {
-    String[] map = new String[]{""," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    String[] nums = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
+        
         List<String> list = new ArrayList();
-        if(digits.length()==0) return list;
-        backtrack(list,digits,new StringBuilder(),0);
+        if(digits.equals(""))return list;
+        helper(digits,list,new StringBuilder(),0);
         return list;
     }
-    void backtrack(List<String> list ,String digits,StringBuilder sb,int length){
+    void helper(String digits,List<String> list,StringBuilder sb,int length){
         if(length==digits.length()){
             list.add(sb.toString());
             return;
         }
+        char c = digits.charAt(length);
+        String s= nums[c-'0'];
         
-        char ch = digits.charAt(length);
-        String s = map[ch-'0'];
         for(int i=0;i<s.length();i++){
             sb.append(s.charAt(i));
-            backtrack(list,digits,sb,length+1);
+            helper(digits,list,sb,length+1);
             sb.deleteCharAt(sb.length()-1);
         }
     }
