974d9cfa0cf0e6cd6ebcbc11ff6a2121110b4c3e
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-10-10 13:30:41 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (42.02%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String breakPalindrome(String s) {
+        if(s.length()==1) return "";
+        char[] c= s.toCharArray();
+        
+        for(int i=0;i<s.length()/2;i++){
+            if(c[i]!='a'){
+               c[i]='a';
+               return new String(c);
+            }
+        }
+        
+        if(c[c.length-1]=='a'){
+            c[c.length-1]='b';
+        }
+        
+        return new String(c);
+    }
+}
