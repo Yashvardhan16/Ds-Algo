65d4e2a8e05ac060e5f01960bc47f10de3efc1ec
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-02 11:51:21 +0530

    Time: 19 ms (6.62%), Space: 45.5 MB (10.93%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minDistance(String a, String b) {
+          int n =a.length();
+        int m = b.length();
+        
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0){
+                    dp[i][j]=j;
+                }else if(j==0){
+                    dp[i][j]=i;
+                }
+                else if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
