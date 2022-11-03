19759f0176dd1344717b1640bd8e76ab95cef489
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-03 10:45:10 +0530

    Time: 24 ms (90.69%), Space: 99.3 MB (68.65%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,11 +1,11 @@
 class Solution {
     public int longestPalindrome(String[] words) {
+        int n = words.length;
         int[][] count = new int[26][26];
         int ans =0;
-        for(String s:words){
-            int a = s.charAt(0)-'a';
-              int b = s.charAt(1)-'a';
-            
+        for(String i:words){
+            int a = i.charAt(0)-'a';
+            int b = i.charAt(1) -'a';
             if(count[b][a]>0){
                 ans+=4;
                 count[b][a]--;
@@ -13,7 +13,6 @@ class Solution {
                 count[a][b]++;
             }
         }
-        
         for(int i=0;i<26;i++){
             if(count[i][i]>0){
                 ans+=2;
