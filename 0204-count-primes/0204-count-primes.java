2e69ceb98304b9129ec9753481b262ff95e558b5
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-11 11:17:36 +0530

    Time: 170 ms (58.40%), Space: 68.1 MB (73.22%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        boolean[] primes = new boolean[n];
+        Arrays.fill(primes,true);
+        int count =0;
+      
+        for(int i=2;i*i<n;i++){
+            if(primes[i]){
+                for(int j=i;j*i<n;j++){
+                    primes[i*j] = false;
+                }
+            }
+        }
+        for(int i=2;i<n;i++){
+            if(primes[i])count++;
+        }
+        return count;
+    }
+    
+}
