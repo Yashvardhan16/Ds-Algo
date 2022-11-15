325f242fbfdab55b253807fc2ea9e97ecb200133
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-15 10:17:13 +0530

    Added solution - LeetHub

---
+++ Longest Perfect Piece - GFG/longest-perfect-piece.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] arr = new int[N];
+            
+            for(int i=0; i<N; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestPerfectPiece(arr,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int longestPerfectPiece(int[] arr, int N) {
+        // code here
+       int max =0,i=0,j=0;
+       
+       while(i<N){
+           if(Math.abs(arr[i]-arr[j])<=1){
+               max = Math.max(max,i-j+1);
+               i++;
+           }else {
+               while(Math.abs(arr[i]-arr[j])>1){
+                   j++;
+               }
+           }
+       }
+       return max;
+    }
+};
