c05312b0d8bf2dc149be4805f67333ea94ad38f5
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-18 09:38:15 +0530

    Time: 2 ms (80.60%), Space: 41.3 MB (43.22%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isUgly(int n) {
-            if(n<=0) return false;
-            if(n==1) return true;
-            if(n%2==0){
-               return isUgly(n/2);
-            }
-            if(n%3==0){
-                return isUgly(n/3);
-            }
-            if(n%5==0){
-return isUgly(n/5);    
-            }
-           
+        if(n<=0) return false;
+        int[] divisors = new int[3];
+        divisors[0] = 2;
+        divisors[1] =3;
+        divisors[2] =5;
         
-        return false;
+        for(int i:divisors){
+            while(n%i==0){
+                n/=i;
+            }
+        }
+        return n==1;
     }
 }
