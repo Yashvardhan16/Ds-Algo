4903ca59192bed29f77e0b718d7185abd76b9072
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-19 12:37:16 +0530

    Time: 174 ms (11.01%), Space: 71.9 MB (14.83%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,26 +1,25 @@
 class Solution {
     public int minDeletions(String s) {
-        if(s.length()==0) return 0;
         Map<Character,Integer> map = new HashMap();
         for(char c:s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(char i:map.keySet()){
-            pq.add(map.get(i));
+       for(char c:map.keySet()) pq.add(map.get(c));
+                // if(pq.size()==1) return 0;
+
+        int count =0;
+        while(pq.size()>1){
+            int top = pq.poll();
+            
+            if(top==pq.peek()){
+                if(top-1>0){
+                    pq.add(top-1);
+                }
+                count++;
+            }
+            
         }
-        if(pq.size()==1) return 0;
-        int delete=0;
-       while(pq.size()>1){
-           int top = pq.poll();
-           
-           if(top==pq.peek()){
-               if(top-1>0){
-                   pq.add(top-1);
-               }
-               delete++;
-           }
-       }
-        return delete;
+        return count;
     }
 }
