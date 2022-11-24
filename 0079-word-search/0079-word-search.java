52da40464a238be6d2deea85b055e7573593f2a1
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-24 10:39:05 +0530

    Time: 135 ms (81.17%), Space: 40 MB (97.38%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,27 +1,28 @@
 class Solution {
-      boolean[][] visited;
+    boolean[][] visited;
     public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
-        
-      visited = new boolean[n][m];
-        
+        visited = new boolean[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j] == word.charAt(0) && helper(board,i,j,0,word)){
+                if(board[i][j]== word.charAt(0) && helper(board,word,i,j,visited,0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    boolean helper(char[][] board,int i,int j,int start,String word){
+    boolean helper(char[][] board,String word,int i,int j, boolean[][] visited,int start){
         if(start==word.length()) return true;
         if(i<0||j<0||i>board.length-1||j>board[0].length-1||board[i][j]!=word.charAt(start)||visited[i][j]) return false;
         
         visited[i][j] = true;
-        if(helper(board,i+1,j,start+1,word)||helper(board,i-1,j,start+1,word)||helper(board,i,j+1,start+1,word)||helper(board,i,j-1,start+1,word)) return true;
-        visited[i][j] =false;
+       if(helper(board,word,i+1,j,visited,start+1)||
+        helper(board,word,i-1,j,visited,start+1)||
+        helper(board,word,i,j+1,visited,start+1)||
+        helper(board,word,i,j-1,visited,start+1)) return true;
+        visited[i][j] = false;
         return false;
     }
 }
