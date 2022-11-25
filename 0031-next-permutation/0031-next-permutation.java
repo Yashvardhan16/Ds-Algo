f71ee7ed2bd1ef816d033b425b22ff8319677c59
Yashvardhan Singh Bhadauria <yashvardhan.singh2019@vitstudent.ac.in>
2022-11-25 13:22:40 +0530

    Time: 2 ms (22.79%), Space: 43.9 MB (38.53%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -12,21 +12,20 @@ class Solution {
             swap(nums,i,j);
         }
         reverse(nums,i+1);
-        
     }
-    void swap(int[] nums,int i,int j){
+    void swap(int [] nums,int i,int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    void reverse(int[] nums,int i){
-        int j = nums.length-1;
-        while(i<j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+    void reverse(int[] nums,int start){
+        int end = nums.length-1;
+        while(start<end){
+             int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
